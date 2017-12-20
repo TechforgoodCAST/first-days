d006e8073551b2f17751505c76504968ab528393
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2017-12-20 10:25:53 +0000

    Add bamboo package for emails

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule FirstSteps.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {FirstSteps.Application, []},
-     extra_applications: [:logger, :runtime_tools]]
+     extra_applications: [:logger, :runtime_tools, :bamboo]]
   end
 
   # Specifies which paths to compile per environment.
@@ -35,7 +35,8 @@ defmodule FirstSteps.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:bamboo, "~> 0.8"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
