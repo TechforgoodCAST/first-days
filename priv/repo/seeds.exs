79d944b0d4d15c24721a8afc8fb676b8f9a4cea3
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-01-05 08:29:04 +0000

    Add seeds script to populate stages db table

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias FirstDays.{State.Stage, Repo}
+alias Ecto.Multi
+
+stages =
+  [
+  "role_description_form",
+  "role_description_template",
+  "confirmation_agreement_template",
+  "document_checklist_form",
+  "document_checklist_template"
+  ]
+  |> Enum.map(fn x -> %{stage: x} end)
+
+
+
+Multi.new
+|> Multi.insert_all(:add_stages, Stage, stages)
+|> Repo.transaction
