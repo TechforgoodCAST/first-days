db908020895fabf0b3ef959f809603f6cfa3422f
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-01-09 18:02:09 +0000

    Add document_checklist fields to schema and flesh out corresponding form

--- lib/first_days/form_data/document_checklist.ex
+++ lib/first_days/form_data/document_checklist.ex
@@ -4,14 +4,36 @@ defmodule FirstDays.DocumentChecklist do
   alias FirstDays.DocumentChecklist
 
   embedded_schema do
-    field :documents, {:array, :string}
+    field :photo_id, :boolean
+    field :proof_of_identity, :boolean
+    field :proof_of_address, :boolean
+    field :volunteer_requirements, :boolean
+    field :emergency_contact_details, :boolean
+    field :dbs_check, :boolean
+    field :proof_of_residency, :boolean
+    field :bank_details, :boolean
+    field :proof_of_qualifications, :boolean
+    field :other_document, :string
   end
+
+  @fields [
+          :photo_id,
+          :proof_of_identity,
+          :proof_of_address,
+          :volunteer_requirements,
+          :emergency_contact_details,
+          :dbs_check,
+          :proof_of_residency,
+          :bank_details,
+          :proof_of_qualifications,
+          :other_document
+          ]
 
   @doc false
   def changeset(%DocumentChecklist{} = document_checklist, attrs) do
     document_checklist
-    |> cast(attrs, [:documents])
-    |> validate_required([:documents])
+    |> cast(attrs, @fields)
+    |> validate_required(List.delete(@fields, :other_document))
   end
 
   def validate_form(%DocumentChecklist{} = struct, params) do
