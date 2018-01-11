f1208adbe6b5690944c0740bf2677b56d3dfe3ad
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-01-11 15:44:29 +0000

    Redirect signed in users to index

--- lib/first_days_web/controllers/page_controller.ex
+++ lib/first_days_web/controllers/page_controller.ex
@@ -1,6 +1,12 @@
 defmodule FirstDaysWeb.PageController do
   use FirstDaysWeb, :controller
   plug :authenticate_user when action in [:first_days_index]
+  alias FirstDays.Accounts.User
+
+  def index(%{assigns: %{current_user: %User{}}} = conn, _params) do
+    conn
+    |> redirect(to: page_path(conn, :first_days_index))
+  end
 
   def index(conn, _params) do
     render conn, "index.html"
