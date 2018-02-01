c0b04a6f0665fefa71c4bf27c69ba5868b7f3a67
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-02-01 18:02:22 +0000

    Send confirmation email on sign up

--- lib/first_days/email.ex
+++ lib/first_days/email.ex
@@ -1,11 +1,12 @@
 defmodule FirstDays.Email do
   use Bamboo.Phoenix, view: FirstDaysWeb.EmailView
 
-  def welcome_email do
+  def welcome_email(%{current_user: current_user}) do
     new_email()
     |> to("ivan@wearecast.org.uk")
     |> from("ivan@wearecast.org.uk")
-    |> subject("Welcome to the service!")
-    |> render(:email)
+    |> subject("Welcome to the First Days!")
+    |> assign(:current_user, current_user)
+    |> render(:welcome_email)
   end
 end
