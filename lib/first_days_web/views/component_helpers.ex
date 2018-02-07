502b4c7ef529fe20fc66dec13cabdc3449a41b0f
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-02-07 15:04:44 +0200

    Add styling to form error tags

--- lib/first_days_web/views/component_helpers.ex
+++ lib/first_days_web/views/component_helpers.ex
@@ -24,4 +24,6 @@ defmodule FirstDaysWeb.ComponentHelpers do
 
   def input_class, do: "pa2 input-reset ba b--blue br2 bg-white measure f5 outline-0 w5"
   def input_label_class, do: "db b f5 lh-copy black mb2"
+  def input_error_class, do: "db f6 red mt1"
+
 end
