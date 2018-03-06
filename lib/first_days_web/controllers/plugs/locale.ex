16d3656164ec6a1ebd96979be2f1e54e873d44a5
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-03-06 13:05:51 +0000

    Update locale controller

--- lib/first_days_web/controllers/plugs/locale.ex
+++ lib/first_days_web/controllers/plugs/locale.ex
@@ -1,13 +1,36 @@
 defmodule FirstDaysWeb.Plugs.Locale do
+  import Plug.Conn
 
   @locales Gettext.known_locales(FirstDaysWeb.Gettext)
 
   def init(_opts), do: nil
 
-  def call(%Plug.Conn{params: %{"locale" => locale}} = conn, _opts) when locale in @locales do
-    Gettext.put_locale(FirstDaysWeb.Gettext, locale)
-    conn
+  def call(conn, _opts) do
+    case locale_from_params(conn) || locale_from_cookies(conn) do
+      nil ->
+        conn
+      locale ->
+        Gettext.put_locale(FirstDaysWeb.Gettext, locale)
+        conn |> persist_locale(locale)
+    end
   end
 
-  def call(conn, _opts), do: conn
+  defp locale_from_params(conn) do
+    conn.params["locale"] |> validate_locale
+  end
+
+  defp locale_from_cookies(conn) do
+    conn.cookies["locale"] |> validate_locale
+  end
+
+  defp validate_locale(locale) when locale in @locales, do: locale
+  defp validate_locale(_locale), do: nil
+
+  defp persist_locale(conn, new_locale) do
+    if conn.cookies["locale"] != new_locale do
+      conn |> put_resp_cookie("locale", new_locale, max_age: 360*24*60*60)
+    else
+      conn
+    end
+  end
 end
--- lib/first_days_web/views/component_view.ex
+++ lib/first_days_web/views/component_view.ex
@@ -2,6 +2,6 @@ defmodule FirstDaysWeb.ComponentView do
   use FirstDaysWeb, :view
 
   def switch_locale_path(conn, locale, language) do
-    "<a href=\"#{page_path(conn, :index, locale: locale)}\">#{language}</a>" |> raw
+    "<a href=\"#{conn.request_path}?locale=#{locale}\">#{language}</a>" |> raw
   end
 end
