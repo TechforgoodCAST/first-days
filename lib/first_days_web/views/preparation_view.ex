51f33490642facf7cbb0560f2051e56bcb11e4f3
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-03-07 12:28:28 +0000

    Add gettext to preparation templates

--- lib/first_days_web/views/preparation_view.ex
+++ lib/first_days_web/views/preparation_view.ex
@@ -11,16 +11,16 @@ defmodule FirstDaysWeb.PreparationView do
     date_select(form, field, [builder: builder] ++ opts)
   end
 
-  def digitsToMonth(1), do: "January"
-  def digitsToMonth(2), do: "February"
-  def digitsToMonth(3), do: "March"
-  def digitsToMonth(4), do: "April"
-  def digitsToMonth(5), do: "May"
-  def digitsToMonth(6), do: "June"
-  def digitsToMonth(7), do: "July"
-  def digitsToMonth(8), do: "August"
-  def digitsToMonth(9), do: "September"
-  def digitsToMonth(10), do: "October"
-  def digitsToMonth(11), do: "November"
-  def digitsToMonth(12), do: "December"
+  def digitsToMonth(1), do: gettext("January")
+  def digitsToMonth(2), do: gettext("February")
+  def digitsToMonth(3), do: gettext("March")
+  def digitsToMonth(4), do: gettext("April")
+  def digitsToMonth(5), do: gettext("May")
+  def digitsToMonth(6), do: gettext("June")
+  def digitsToMonth(7), do: gettext("July")
+  def digitsToMonth(8), do: gettext("August")
+  def digitsToMonth(9), do: gettext("September")
+  def digitsToMonth(10), do: gettext("October")
+  def digitsToMonth(11), do: gettext("November")
+  def digitsToMonth(12), do: gettext("December")
 end
